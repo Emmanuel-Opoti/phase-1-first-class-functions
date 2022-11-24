b8600efc3128218d39968d170764bf50bf4427a5
Emmanuel Opoti <opoti63@gmail.com>
2022-11-24 08:03:51 +0300

    Complete Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback){
+    return callback();
+}
+
+function returnsANamedFunction(){
+    const named = function(){};
+    return named;
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){};
+}
+
